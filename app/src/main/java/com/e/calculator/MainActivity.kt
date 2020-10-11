af01f57d8028d95da6952976b6c3807363c01468
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-10-11 19:25:05 +0100

    add solution

--- app/src/main/java/com/e/calculator/MainActivity.kt
+++ app/src/main/java/com/e/calculator/MainActivity.kt
@@ -2,10 +2,58 @@ package com.e.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.e.calculator.utils.ClickHandler
 
 class MainActivity : AppCompatActivity() {
+
+    private fun addClicks(buttons: Array<Int>, expressionField: TextView) {
+        val handler = ClickHandler(expressionField)
+
+        buttons.forEach { button ->
+            findViewById<Button>(button).setOnClickListener {
+                when (button) {
+                    R.id.button0 -> handler.buttonDigitClick('0')
+                    R.id.button1 -> handler.buttonDigitClick('1')
+                    R.id.button2 -> handler.buttonDigitClick('2')
+                    R.id.button3 -> handler.buttonDigitClick('3')
+                    R.id.button4 -> handler.buttonDigitClick('4')
+                    R.id.button5 -> handler.buttonDigitClick('5')
+                    R.id.button6 -> handler.buttonDigitClick('6')
+                    R.id.button7 -> handler.buttonDigitClick('7')
+                    R.id.button8 -> handler.buttonDigitClick('8')
+                    R.id.button9 -> handler.buttonDigitClick('9')
+
+                    R.id.buttonPlus -> handler.buttonOperationClick('+')
+                    R.id.buttonMinus -> handler.buttonOperationClick('-')
+                    R.id.buttonMultiply -> handler.buttonOperationClick('*')
+                    R.id.buttonDivide -> handler.buttonOperationClick('/')
+
+                    R.id.buttonOpenBracket -> handler.buttonOpenBracketClick()
+                    R.id.buttonCloseBracket -> handler.buttonCloseBracketClick()
+                    R.id.buttonPoint -> handler.buttonPointClick()
+                    R.id.buttonEqual -> handler.buttonEqualClick()
+                    R.id.buttonC -> handler.buttonCClick()
+                    R.id.buttonCE -> handler.buttonCEClick()
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val expressionField = findViewById<TextView>(R.id.expressionField)
+        val buttons = arrayOf(
+                R.id.button1, R.id.button2, R.id.button3, R.id.buttonPlus,
+                R.id.button4, R.id.button5, R.id.button6, R.id.buttonMinus,
+                R.id.button7, R.id.button8, R.id.button9, R.id.buttonMultiply,
+                R.id.buttonOpenBracket, R.id.button0, R.id.buttonCloseBracket, R.id.buttonDivide,
+                R.id.buttonPoint, R.id.buttonEqual, R.id.buttonC, R.id.buttonCE
+        )
+
+        addClicks(buttons, expressionField)
     }
 }
---
+++ app/src/main/java/com/e/calculator/utils/Parser.kt
@@ -0,0 +1,124 @@
+package com.e.calculator.utils
+
+import java.lang.ArithmeticException
+import java.util.*
+
+private fun getItems(expression: String): List<String> {
+    var current = ""
+    val result = mutableListOf<String>()
+
+    expression.forEachIndexed { index, it ->
+        when(it) {
+            in '0'..'9' -> current += it
+            '.' -> current += it
+            else -> {
+                if (it == '-' && (index == 0 || expression[index - 1] == '(')) {
+                    current += it
+                } else {
+                    if (current.isNotEmpty()) result.add(current)
+
+                    result.add(it.toString())
+
+                    current = ""
+                }
+            }
+        }
+    }
+
+    if (current.isNotEmpty()) result.add(current)
+
+    return result
+}
+
+private fun removeUseLessBrackets(items: List<String>): List<String> {
+    val result = mutableListOf<String>()
+
+    var shouldRemove = false
+
+    items.forEachIndexed { index, it ->
+        if (it == "(" && items[index + 2] == ")") shouldRemove = true
+        else if (it == ")" && shouldRemove) shouldRemove = false
+        else result.add(it)
+    }
+
+    return result
+}
+
+private fun example(numberStack: Stack<Double>, operationStack: Stack<Char>, operation: Char) {
+    if (operation == '+' || operation == '-') {
+        while(!operationStack.empty() && (operationStack.peek() == '+' || operationStack.peek() == '-' || operationStack.peek() == '*' || operationStack.peek() == '/')) {
+            val second = numberStack.pop()
+            val first = numberStack.pop()
+            when(operationStack.pop()) {
+                '+' -> numberStack.push(first + second)
+                '-' -> numberStack.push(first - second)
+                '*' -> numberStack.push(first * second)
+                '/' -> if (second != 0.0) numberStack.push(first / second) else TODO()
+            }
+        }
+    } else {
+        while(!operationStack.empty() &&
+            (operationStack.peek() == '*' || operationStack.peek() == '/')) {
+            val second = numberStack.pop()
+            val first = numberStack.pop()
+            when(operationStack.pop()) {
+                '*' -> numberStack.push(first * second)
+                '/' -> if (second != 0.0) numberStack.push(first / second) else TODO()
+            }
+        }
+    }
+
+    operationStack.push(operation)
+}
+
+private fun example1(numberStack: Stack<Double>, operationStack: Stack<Char>) {
+    while (!operationStack.empty() && operationStack.peek() != '(') {
+        val second = numberStack.pop()
+        val first = numberStack.pop()
+        when(operationStack.pop()) {
+            '+' -> numberStack.push(first + second)
+            '-' -> numberStack.push(first - second)
+            '*' -> numberStack.push(first * second)
+            '/' -> if (second != 0.0) numberStack.push(first / second) else throw ArithmeticException("Division by zero attempt.")
+        }
+    }
+
+    operationStack.pop()
+}
+
+private fun calculate(items: List<String>): Double {
+    val numberStack = Stack<Double>()
+    val operationStack = Stack<Char>()
+
+    items.forEach {
+        when {
+            it.toDoubleOrNull() != null -> numberStack.push(it.toDouble())
+            it == "(" -> operationStack.push(it[0])
+            it == ")" -> example1(numberStack, operationStack)
+            it == "+" || it == "-" || it == "*" || it == "/" -> example(numberStack, operationStack, it[0])
+        }
+    }
+
+    while(!operationStack.empty()) {
+        val second = numberStack.pop()
+        val first = numberStack.pop()
+
+        when(operationStack.pop()) {
+            '+' -> numberStack.push(first + second)
+            '-' -> numberStack.push(first - second)
+            '*' -> numberStack.push(first * second)
+            '/' -> if (second != 0.0) numberStack.push(first / second) else TODO()
+        }
+    }
+
+    return numberStack.pop()
+}
+
+class Parser {
+
+    fun parse(expression: String): Double {
+        val items = removeUseLessBrackets(getItems(expression))
+
+        return calculate(items)
+    }
+}
